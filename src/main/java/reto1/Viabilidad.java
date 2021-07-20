b0368ac75b6171a2c1a255d8322ffa75abc7989d
solorzanojf <solorzanojf@gmail.com>
2021-07-20 12:13:16 -0500

    inicio de reto

---
+++ src/main/java/reto1/Viabilidad.java
@@ -0,0 +1,12 @@
+package reto1;
+
+public class Viabilidad {
+    public static void main( String[] args ) {
+
+        int pTiempo=0;
+        double pCapital=0;
+        double pInteres=0;
+        
+
+    }
+}
